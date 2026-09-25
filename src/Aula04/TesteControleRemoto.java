cf5c4edfe85f8f4f053163f8f9332f3a7774d2bd
Felipeaards <felipeaards@gmail.com>
2026-09-24 23:18:37 -0300

    Adicao aula 04

---
+++ src/Aula04/TesteControleRemoto.java
@@ -0,0 +1,57 @@
+package Aula04;
+
+public class TesteControleRemoto {
+    public static void main(String[] args){
+        //Criar um objeto do Controle Remoto
+        ControleRemoto controle1 = new ControleRemoto();
+        System.out.println("-------- Controle Remoto criado --------");
+        System.out.println("Estado: "+ controle1.getStatus());
+        System.out.println("Volume: "+ controle1.getVolume());
+        System.out.println("----------------------------------------");
+
+        System.out.println("Teste 01");
+        controle1.setVolume(45);
+        System.out.println("Volume: "+ controle1.getVolume());
+        System.out.println("Teste 02");
+        System.out.println("----------------------------------------");
+        controle1.ligar();
+        System.out.println("Teste 03");
+        System.out.println("----------------------------------------");
+        controle1.aumentar();
+        controle1.ligar();
+        controle1.aumentar();
+        controle1.ligar();
+        controle1.aumentar();
+        controle1.aumentar();
+        controle1.diminuir();
+        controle1.diminuir();
+        controle1.diminuir();
+        System.out.println("Teste 04");
+        System.out.println("----------------------------------------");
+        controle1.setCanal(499);
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.diminuirCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+        controle1.aumentarCanal();
+
+    }
+}
